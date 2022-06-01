732b7cc208e4648b2436fd3cd783ef4bd7686b03
tugcetaskin <106422014+tugcetaskin@users.noreply.github.com>
2022-06-01 16:44:02 +0300

    Add files via upload

---
+++ Sayı Sıralama/src/SayiSiralama.java
@@ -0,0 +1,46 @@
+import  java.util.Scanner;
+
+public class SayiSiralama {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int a, b, c;
+        System.out.println("1. sayıyı giriniz:");
+        a= input.nextInt();
+
+        System.out.println("2. sayıyı giriniz:");
+        b= input.nextInt();
+
+        System.out.println("3. sayıyı giriniz:");
+        c= input.nextInt();
+
+        if ((a>b) && (a>c)) {
+
+            if (b>c) {
+                System.out.println(a +">" +b +">"+ c);
+            }else {
+                System.out.println(a +">" +c +">"+ b);
+            }
+
+        }else if ((b>a) && (b>c)) {
+
+            if (a>c) {
+                System.out.println(b +">" +a +">"+ c);
+            }else {
+                System.out.println(b +">" +c +">"+ a);
+            }
+
+        }else if ((c>a) && (c>b)) {
+
+            if (a>b) {
+                System.out.println(c +">" +a +">"+ b);
+            }else {
+                System.out.println(c +">" +b +">"+ a);
+            }
+        }
+
+
+
+    }
+}
