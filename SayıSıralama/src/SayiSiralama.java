8a114c0dd342fd93570672328c728fd65162b818
tugcetaskin <106422014+tugcetaskin@users.noreply.github.com>
2022-06-03 20:48:04 +0300

    Add files via upload

---
+++ SayıSıralama/src/SayiSiralama.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SayiSiralama {
+    public static void main(String[] args) {
+
+        int sayac,sayi,min=0,max=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kac sayi gireceksiniz : ");
+        sayac = input.nextInt();
+
+        for(int i=1; i<=sayac;i++){
+            System.out.print(i + ". Sayiyi giriniz : "  );
+            sayi = input.nextInt();
+
+            if (i==1){
+                max=sayi;
+                min=sayi;
+            }else{
+                if (sayi>max){
+                    max=sayi;
+                } else if (sayi<min) {
+                    min=sayi;
+                }
+            }
+        }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+    }
+}
